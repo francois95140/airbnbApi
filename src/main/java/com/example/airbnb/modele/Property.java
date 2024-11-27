b62e0db6940399db7902fa6b256f5734753dcae2
François <totofrancois12@gmail.com>
2024-11-27 23:16:32 +0100

    Using uuid

--- src/main/java/com/example/airbnb/modele/Property.java
+++ src/main/java/com/example/airbnb/modele/Property.java
@@ -6,11 +6,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Entity
 public class Property {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
     private String name;
     private String description;
     private int price;

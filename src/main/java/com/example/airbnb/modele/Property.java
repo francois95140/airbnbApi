609cdde11f9a2cf11160d6bd0c1dffc4437d727d
François <totofrancois12@gmail.com>
2024-11-29 21:35:57 +0100

    add tracking column

--- src/main/java/com/example/airbnb/modele/Property.java
+++ src/main/java/com/example/airbnb/modele/Property.java
@@ -2,18 +2,36 @@ package com.example.airbnb.modele;
 
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
+@Table(name = "PROPERTY")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Getter
+@Setter
 public class Property {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    @NonNull
     private String name;
+    @NonNull
     private String description;
-    private int price;
+
+    private Double pricePerNight;
+
+    @CreationTimestamp
+    private LocalDateTime creatAt ;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
 }
